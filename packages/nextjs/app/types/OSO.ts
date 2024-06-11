14bccad3973e70d5d3d30668dad3ed4bd39f0253
escottalexander@gmail.com <escottalexander@gmail.com>
2024-06-11 12:23:02 -0400

    stub out etl endpoint

--- packages/nextjs/app/types/OSO.ts
+++ packages/nextjs/app/types/OSO.ts
@@ -1,47 +1,26 @@
 export interface OSOResponse {
-  onchain_metrics_by_project: OnchainMetricsByProject[];
-  code_metrics_by_project: CodeMetricsByProject[];
+  onchain_metrics_by_project_v1: OnchainMetricsByProject[];
 }
 
 export interface OnchainMetricsByProject {
-  active_users: number;
-  first_txn_date: string;
-  high_frequency_users: number;
-  l2_gas_6_months: number;
-  less_active_users: number;
-  more_active_users: number;
-  multi_project_users: number;
-  network: string;
-  new_user_count: number;
-  num_contracts: number;
+  active_contract_count_90_days: number;
+  address_count: number;
+  address_count_90_days: number;
+  days_since_first_transaction: number;
+  display_name: string;
+  event_source: string;
+  gas_fees_sum: number;
+  gas_fees_sum_6_months: number;
+  high_activity_address_count_90_days: number;
+  low_activity_address_count_90_days: number;
+  medium_activity_address_count_90_days: number;
+  multi_project_address_count_90_days: number;
+  new_address_count_90_days: number;
   project_id: string;
   project_name: string;
-  total_l2_gas: number;
-  total_txns: number;
-  total_users: number;
-  txns_6_months: number;
-  users_6_months: number;
+  project_namespace: string;
+  project_source: string;
+  returning_address_count_90_days: number;
+  transaction_count: number;
+  transaction_count_6_months: number;
 }
-
-export interface CodeMetricsByProject {
-  avg_active_devs_6_months: number;
-  avg_fulltime_devs_6_months: number;
-  commits_6_months: number;
-  contributors: number;
-  contributors_6_months: number;
-  first_commit_date: string;
-  forks: number;
-  issues_closed_6_months: number;
-  issues_opened_6_months: any;
-  last_commit_date: string;
-  new_contributors_6_months: number;
-  project_id: string;
-  project_name: string;
-  pull_requests_merged_6_months: number;
-  pull_requests_opened_6_months: number;
-  repositories: number;
-  source: string;
-  stars: number;
-}
-
-export type OSOResponseProps = OnchainMetricsByProject | CodeMetricsByProject;
