1461b1cb2000faeef583bdc12f7f2de80b13b6ca
Kcpele <fidekg123@gmail.com>
2024-06-21 01:40:27 +0100

    updated navbar

--- packages/nextjs/components/Navbar.tsx
+++ packages/nextjs/components/Navbar.tsx
@@ -4,12 +4,12 @@ import { SwitchTheme } from "./SwitchTheme";
 
 const Navbar = () => {
   return (
-    <nav className="max-w-screen-2xl mx-auto flex justify-between items-center p-2">
+    <nav className="navbar p-2 bg-base-100 max-w-screen-2xl mx-auto flex justify-between items-center ">
       <div className="flex gap-2 items-center">
         <div className="max-w-[24px]">
           <Image src="/assets/svg/logo.svg" width={24} height={24} className="w-full" alt="logo" />
         </div>
-        <h3 className="text-neutral-900  font-bold text-sm sm:text-xl  m-0">Retro Funding</h3>
+        <h3 className="text-op-neutral-900  font-bold text-sm sm:text-xl  m-0">Retro Funding</h3>
       </div>
       <div className="flex items-center gap-3">
         <SwitchTheme />
