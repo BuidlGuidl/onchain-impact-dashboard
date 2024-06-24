94b2b6a11447e1cd3574e601eae1ce63829a72be
Kcpele <fidekg123@gmail.com>
2024-06-24 22:37:05 +0100

    revert:navbar logo image and text

--- packages/nextjs/components/Navbar.tsx
+++ packages/nextjs/components/Navbar.tsx
@@ -4,10 +4,13 @@ import { SwitchTheme } from "./SwitchTheme";
 
 const Navbar = () => {
   return (
-    <nav className="navbar p-2 bg-base-100  ">
-      <div className="max-w-screen-2xl w-full mx-auto flex justify-between items-center ">
-        <div className="max-w-[167px]">
-          <Image src="/assets/svg/logo.svg" width={167} height={24} className="w-fit" alt="logo" />
+    <nav className="navbar p-2 sm:px-6 bg-base-100  ">
+      <div className="w-full flex justify-between items-center ">
+        <div className="flex gap-2 items-center">
+          <div className="max-w-[24px]">
+            <Image src="/assets/svg/logo.svg" width={24} height={24} className="w-full" alt="logo" />
+          </div>
+          <h3 className="text-op-neutral-900  font-bold text-sm sm:text-xl  m-0">Retro Funding</h3>
         </div>
         <div className="flex items-center gap-3">
           <SwitchTheme />
