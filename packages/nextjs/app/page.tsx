87a52c91dcd122fd558cb93e7a96ac05e67b30f2
Kcpele <fidekg123@gmail.com>
2024-06-26 19:16:19 +0100

    removed useState

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,12 +1,7 @@
-"use client";
-
-import { useState } from "react";
 import "./page.css";
 import type { NextPage } from "next";
-import OnchainInfoModal from "~~/components/OnchainInfoModal";
 
 const Leaderboard: NextPage = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <section className="px-4">
       <h1>Onchain Impact Dashboard</h1>
@@ -15,12 +10,6 @@ const Leaderboard: NextPage = () => {
         See the onchain impact that is beeing generated across the superchain Retro Funding 4 established metrics to
         measure onchain impact. These metrics will be evolve round over round.
       </p>
-      {/* to be removed after approval */}
-      <button className="bg-OPred text-white p-2 rounded-md" onClick={() => setIsModalOpen(true)}>
-        Test Modal
-      </button>
-
-      <OnchainInfoModal isModalOpen={isModalOpen} closeModal={() => setIsModalOpen(false)} />
     </section>
   );
 };
