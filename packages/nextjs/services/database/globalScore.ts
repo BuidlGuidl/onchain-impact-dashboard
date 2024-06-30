67f75294735eb6fd14e547434fa66bd4d0c4e1f0
Fran <franciscopa91@gmail.com>
2024-06-30 17:51:56 +0200

    fix seed.sample

--- packages/nextjs/services/database/globalScore.ts
+++ packages/nextjs/services/database/globalScore.ts
@@ -7,9 +7,10 @@ const collectionName = "globalScore";
 const globalScoreDoc = (id: string) => firestoreDB.collection(collectionName).doc(id);
 const globalScoreDocs = () => firestoreDB.collection(collectionName);
 
-const globalScoreFilteredByDate = (date: string) => firestoreDB.collection(collectionName).where("__name__", ">", date);
+const globalScoreFilteredByDate = (date: string) =>
+  firestoreDB.collection(collectionName).where("__name__", ">=", date);
 const globalScoreFiltered = (date: [string, string]) =>
-  globalScoreFilteredByDate(date[0]).where("__name__", "<", date[1]);
+  globalScoreFilteredByDate(date[0]).where("__name__", "<=", date[1]);
 
 export const getGlobalScoreFilteredDate = (date: string) => globalScoreFilteredByDate(date).get();
 export const getGlobalScoreFiltered = (dates: [string, string]) => globalScoreFiltered(dates).get();
