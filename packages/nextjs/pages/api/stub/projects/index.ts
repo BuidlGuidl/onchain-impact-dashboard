0a7078c37d9471842f07ecdf3fc4443e59fe3015
escottalexander@gmail.com <escottalexander@gmail.com>
2024-07-01 16:31:04 -0400

    change stub api to paginate with matched response to Agora

--- packages/nextjs/pages/api/stub/projects/index.ts
+++ packages/nextjs/pages/api/stub/projects/index.ts
@@ -8,17 +8,28 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
   const projects = projectsJSON as Project[];
   // check if id param is present
-  const { id } = req.query;
-
+  const { limit = "10", offset = "0" } = req.query;
+  const parseLimit: number = parseInt(limit as string);
+  const parsedOffset: number = parseInt(offset as string);
   try {
-    if (id) {
-      const project = projects.find(project => project.attestationUid === id);
-      if (!project) {
-        return res.status(404).json({ message: "Project not found. Make sure you are URL encoding the id" });
-      }
-      return res.status(200).json(project);
+    if (parseLimit && parseLimit > 100) {
+      return res.status(500).json({
+        code: "custom",
+        message: "Invalid input",
+        path: [],
+      });
     }
-    res.status(200).json(projects);
+    const paginatedProjects = projects.slice(parsedOffset, Math.min(parsedOffset + parseLimit, projects.length));
+    const hasNext = projects.length > parsedOffset + parseLimit;
+    const response = {
+      meta: {
+        has_next: hasNext,
+        total_returned: paginatedProjects.length,
+        next_offset: hasNext ? parsedOffset + parseLimit : 0,
+      },
+      data: paginatedProjects,
+    };
+    res.status(200).json(response);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal Server Error" });
