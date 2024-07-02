c00da51c43acfaf86ad48ec8b8457dc86e2418f9
Sam Wellander <samwellander@gmail.com>
2024-07-02 13:05:45 -0700

    add `getProjectIds()` project service util func

--- packages/nextjs/services/onchainImpactDashboardApi/projectService.ts
+++ packages/nextjs/services/onchainImpactDashboardApi/projectService.ts
@@ -1,21 +1,28 @@
 import { Project } from "../database/schema";
 
 export const ProjectService = () => {
-  const baseURL = `${process.env.NEXT_PUBLIC_API_URL}/stub/projects`;
+  const baseURL = process.env.NEXT_PUBLIC_API_URL;
   const getPaginatedProjects = async () => {
-    const response = await fetch(`${baseURL}?limit=100`);
+    const response = await fetch(`${baseURL}/stub/projects?limit=100`);
     const data = await response.json();
     return data.data as Project[];
   };
 
   const getProjectById = async (id: string) => {
-    const response = await fetch(`${baseURL}?id=${id}`);
+    const response = await fetch(`${baseURL}/stub/projects?id=${id}`);
     const { data }: { data: Project } = await response.json();
+    return data;
+  };
+
+  const getProjectIds = async () => {
+    const response = await fetch(`${baseURL}/projects/ids`);
+    const data: string[] = await response.json();
     return data;
   };
 
   return {
     getPaginatedProjects,
     getProjectById,
+    getProjectIds,
   };
 };
