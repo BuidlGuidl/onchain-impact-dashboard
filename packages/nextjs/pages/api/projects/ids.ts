e1df2009f884ad52941e658389290ccefa7f85a0
Sam Wellander <samwellander@gmail.com>
2024-07-02 13:06:21 -0700

    pull project ids from db
    which uses OSO ids

--- packages/nextjs/pages/api/projects/ids.ts
+++ packages/nextjs/pages/api/projects/ids.ts
@@ -1,14 +1,12 @@
-import projectsJSON from "../stub/projects/projects.json";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { Project } from "~~/services/database/schema";
+import { getProjectIds } from "~~/services/database/projects";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "GET") {
     return res.status(405).json({ message: "Method not allowed." });
   }
   try {
-    const projects = projectsJSON as Project[];
-    const ids = projects.map(({ id }) => id);
+    const ids = await getProjectIds();
     res.status(200).json(ids);
   } catch (error) {
     console.error(error);
