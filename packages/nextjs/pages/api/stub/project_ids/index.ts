993a4e49da144933d8d84f61525579d43879e11f
Sam Wellander <samwellander@gmail.com>
2024-07-01 18:04:08 -0700

    add route handler for `/api/stub/project_ids`

---
+++ packages/nextjs/pages/api/stub/project_ids/index.ts
@@ -0,0 +1,17 @@
+import projectsJSON from "../projects/projects.json";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Project } from "~~/services/database/schema";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "GET") {
+    return res.status(405).json({ message: "Method not allowed." });
+  }
+  try {
+    const projects = projectsJSON as Project[];
+    const ids = projects.map(({ id }) => id);
+    res.status(200).json(ids);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
