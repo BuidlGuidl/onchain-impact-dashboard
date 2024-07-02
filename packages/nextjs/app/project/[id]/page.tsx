1b2d5a92a726bbe01472239f50e0d174960c8fa2
Fran <franciscopa91@gmail.com>
2024-07-02 03:08:46 +0200

    fixes on ui and add seed data

--- packages/nextjs/app/project/[id]/page.tsx
+++ packages/nextjs/app/project/[id]/page.tsx
@@ -18,12 +18,18 @@ const ProjectDetail: NextPage<{ params: { id: string } }> = async ({ params }) =
   return (
     <>
       <section className="px-4">
-        <div className="w-full h-[200px] bg-OPlightgray mb-4 rounded-lg"></div>
+        <Image
+          width={2000}
+          height={440}
+          className="mr-0 w-full bg-OPlightgray mb-4 rounded-lg"
+          src={data.proejctCoverImageUrl}
+          alt="Avatar"
+        />
+
         <div className="flex w-full justify-between mb-4 items-center">
-          <Image width={54} height={54} className="mr-2" src={data.profileAvatarUrl} alt="Avatar" />
+          <Image width={54} height={54} className="mr-0" src={data.profileAvatarUrl} alt="Avatar" />
           <div className="flex flex-col flex-1 ml-4 justify-center">
             <h1 className="text-lg m-0">#1 {data?.name}</h1>
-            <span>{data?.description}</span>
           </div>
           <CustomButton text={"Share"} customClassName="border bg-transparent border-gray-200">
             <ShareIcon />
