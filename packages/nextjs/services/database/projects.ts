4ff58c241977fe62833572b14be60c6f60379f4f
Sam Wellander <samwellander@gmail.com>
2024-07-02 13:04:12 -0700

    add `getProjectIds()` util function
    to services/database

--- packages/nextjs/services/database/projects.ts
+++ packages/nextjs/services/database/projects.ts
@@ -7,3 +7,9 @@ const collectionName = "projects";
 const getProjectDoc = (id: string) => firestoreDB.collection(collectionName).doc(id);
 export const getProjectById = (id: string) => getProjectDoc(id).get();
 export const addProjectDoc = (project: Project) => addDocumentToCollection(collectionName, project);
+
+export const getProjectIds = async () => {
+  const snapshot = await firestoreDB.collection(collectionName).get();
+  const ids = snapshot.docs.map(doc => doc.id);
+  return ids;
+};
