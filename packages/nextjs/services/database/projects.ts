9577db6ba56b9cbd1ddc2435b1f8b7e43777805f
Sam Wellander <samwellander@gmail.com>
2024-07-03 14:17:11 -0700

    update db service helper methods

--- packages/nextjs/services/database/projects.ts
+++ packages/nextjs/services/database/projects.ts
@@ -5,11 +5,21 @@ import { Project } from "./schema";
 const firestoreDB = getFirestoreConnector();
 const collectionName = "projects";
 const getProjectDoc = (id: string) => firestoreDB.collection(collectionName).doc(id);
-export const getProjectById = (id: string) => getProjectDoc(id).get();
+const getProjectsSnapshot = () => firestoreDB.collection(collectionName).get();
 export const addProjectDoc = (project: Project) => addDocumentToCollection(collectionName, project);
 
 export const getProjectIds = async () => {
   const snapshot = await firestoreDB.collection(collectionName).get();
   const ids = snapshot.docs.map(doc => doc.id);
   return ids;
+};
+
+export const getProjectById = async (id: string) => {
+  const snapshot = await getProjectDoc(id).get();
+  return { id: snapshot.id, ...snapshot.data() } as Project;
+};
+
+export const getProjects = async () => {
+  const snapshot = await getProjectsSnapshot();
+  return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() } as Project));
 };
