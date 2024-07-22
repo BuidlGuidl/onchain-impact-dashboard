e0d06f9a076260938c0cf31b4615b83231d678dc
escottalexander@gmail.com <escottalexander@gmail.com>
2024-07-22 12:52:25 -0400

    add maxDuration to function

--- packages/nextjs/pages/api/etl/index.ts
+++ packages/nextjs/pages/api/etl/index.ts
@@ -10,6 +10,10 @@ import Project from "~~/services/mongodb/models/project";
 import ProjectMovement, { IProjectMovement, TempProjectMovement } from "~~/services/mongodb/models/projectMovement";
 import ProjectScore, { IProjectScore, TempProjectScore } from "~~/services/mongodb/models/projectScore";
 
+export const config = {
+  maxDuration: 60, // seconds
+};
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "GET") {
     return res.status(405).json({ message: "Method not allowed." });
